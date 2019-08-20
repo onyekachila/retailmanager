32044b9f4f5c9748e5fdbf90921e5e5cdd612e0b
onyekachila <onyekachila@gmail.com>
2019-08-20 11:47:21 +0100

    Register Database Tables
    The tables for the basic table register system

---
+++ RMData/dbo/Tables/User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	 
+    [Id] NCHAR(128) NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(256) NOT NULL, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+	
+)
